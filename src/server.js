6d92585843c76bd97781a590048b65b587e967f0
dongwenxiao <cs_victor@126.com>
2017-02-06 11:53:31 +0800

    use new verson of sp-react-isomophic

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import { middleware, router, run, commonMiddlewares } from 'sp-base/server'
 import { router as reactRouter, createConfigureStore } from './client'
 import { template } from './html'
+import isomorphic from 'sp-react-isomorphic'
 
 // koa middleware
 
@@ -9,7 +10,6 @@ import { template } from './html'
 commonMiddlewares(middleware)
 
 // react 同构中间件
-const isomorphic = require('sp-react-isomorphic')
 const configureStore = createConfigureStore()
 middleware.use(isomorphic(reactRouter.get(), configureStore, template))
 
