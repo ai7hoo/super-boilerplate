8a2cff4ff76417518328728b7696c30d68b85912
Diablohu <diablohudream@gmail.com>
2017-03-17 16:39:29 +0800

    DocContainer

---
+++ docs/pages/components/concept/zh-cn.md
@@ -0,0 +1,3 @@
+## 基础理念
+
+Occaecat irure ea sit laboris reprehenderit mollit veniam occaecat est adipisicing laboris tempor do minim. Mollit aliquip adipisicing commodo ipsum amet anim fugiat. Eiusmod exercitation minim excepteur do fugiat tempor ad labore commodo anim nostrud. Sit culpa amet tempor excepteur incididunt nostrud anim Lorem id aute laborum. In do elit dolore sunt nostrud. Amet sunt in elit deserunt adipisicing tempor consectetur irure minim nulla ex ipsum eiusmod proident. Ut sint labore deserunt sunt laboris enim Lorem nisi commodo sint veniam fugiat do amet.
---
+++ src/client/ui/containers/DocContainer.jsx
@@ -0,0 +1,98 @@
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+import PageContainer from './PageContainer.jsx'
+import { DOC_GET_CONTENT } from '../../redux/action-types'
+
+import { ImportStyle } from 'sp-css-import'
+import style from './DocContainer.less'
+
+let thisDoc
+
+export const reducer = (state = {}, action) => {
+    switch (action.type) {
+        case DOC_GET_CONTENT:
+            if (!action.doc) return
+            var o = {}
+            o[action.doc] = action.content
+            return Object.assign({}, state, o)
+    }
+    return state
+}
+
+const getContent = (doc, localeId) => {
+    return (dispatch) => {
+        // if (store && store.requestData) {
+        //     return dispatch(timePassed(store.requestData))
+        // }
+
+        return dispatch(dispatch => {
+            return import(`Docs/${doc}/${localeId}.md`)
+                .then(data => {
+                    return dispatch({
+                        type: DOC_GET_CONTENT,
+                        doc: doc,
+                        content: data
+                    })
+                }).catch(function (error) {
+                    dispatch({
+                        type: DOC_GET_CONTENT,
+                        doc: doc,
+                        content: 'ERROR'
+                    })
+                })
+        })
+    }
+}
+
+@connect((state) => ({
+    localeId: state.localeId,
+    content: state.docs[thisDoc],
+    isLoading: state.docs[thisDoc] ? false : true
+}))
+@ImportStyle(style)
+class DocContainer extends React.Component {
+    static preprocess(state, dispatch) {
+        const preprocessTasks = []
+        preprocessTasks.push(
+            dispatch(getContent(thisDoc, state.localeId))
+        )
+        return preprocessTasks
+    }
+
+    renderContent() {
+        if (!this.props.content && __CLIENT__) {
+            this.isClientRender = true
+            this.props.dispatch(getContent(thisDoc, this.props.localeId))
+            return ''
+        } else {
+            return (
+                <div>
+                    {this.props.content}
+                </div>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <PageContainer
+                className={this.props.className}
+                isLoading={this.props.isLoading}
+            >
+                {this.renderContent()}
+            </PageContainer>
+        )
+    }
+}
+
+export const register = (doc) => {
+    thisDoc = doc
+}
+
+export const getComponent = (doc) => {
+    register(doc)
+    return DocContainer
+}
+
+export default DocContainer
