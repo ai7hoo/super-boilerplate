61fc7e00ffc63dbc13ca7db251c5072a554f21bc
Diablohu <diablohudream@gmail.com>
2017-04-21 15:27:07 +0800

    update scripts

--- deploy.js
+++ deploy.js
@@ -42,9 +42,9 @@ const run = () => {
     })
 
         .then(() => runScript('npm install', '[NPM] install'))
-        .then(() => runScript('npm run build', '[NPM] build'))
+        // .then(() => runScript('npm run build', '[NPM] build'))
         // .then(() => runScript('pm2 delete sp-boilerplate', '[PM2] kill service'))
-        .then(() => runScript('npm run start:pm2', '[PM2] starting server'))
+        .then(() => runScript('npm run start:pm2', '[NPM] build & [PM2] starting server'))
 
         .then(() => {
             console.log('ALL DONE!')
