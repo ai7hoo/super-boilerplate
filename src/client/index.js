62940f6a9d3ede9da98ac3857ee8fc2dc64fd692
Diablohu <diablohudream@gmail.com>
2017-06-12 10:56:23 +0800

    update dependencies

--- src/client/index.js
+++ src/client/index.js
@@ -71,7 +71,7 @@ if (__SERVER__) {
 
 //
 if (__CLIENT__) {
-    const store = run({
+    /*const store = */run({
         browserHistoryOnUpdate: (location) => {
             // 回调: browserHistoryOnUpdate
             // 正常路由跳转时，URL发生变化后瞬间会触发，顺序在react组件读取、渲染之前
