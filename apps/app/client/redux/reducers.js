8cfc869f04d529afa740b780493fe7eedce63a53
dongwenxiao <cs_victor@126.com>
2017-10-17 12:43:39 +0800

    fix server reducer != client reducer

--- apps/app/client/redux/reducers.js
+++ apps/app/client/redux/reducers.js
@@ -1,5 +1,7 @@
 import docReducer from '@appLogic/doc/reducer'
+import { SERVER_REDUCER_NAME, serverReducer } from '@app/server/server-redux'
 
 export default [
     ['docs', docReducer],
+    [SERVER_REDUCER_NAME, serverReducer]
 ]
