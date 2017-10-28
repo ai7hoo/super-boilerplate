1dc9aa5491fd739c97fc361be279a6d07849b878
Diablohu <diablohudream@gmail.com>
2017-10-28 14:45:16 +0800

    dev spa

--- system/webpack/client/dev.spa.js
+++ system/webpack/client/dev.spa.js
@@ -68,7 +68,11 @@ const factoryConfig = async(opt) => {
                 // filename: '../index.html',
                 filename: 'index.html',
                 template: path.resolve(RUN_PATH, `./apps/${APP_KEY}/html.ejs`),
-                inject: 'head'
+                inject: 'body',
+                excludeChunks: [
+                    'critical'
+                ],
+                __DEV__: true
             })
         ],
         resolve: common.resolve
