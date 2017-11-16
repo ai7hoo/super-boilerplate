18f2eb238dab0b10b1324c60022c28f639495902
dongwenxiao <cs_victor@126.com>
2017-11-16 13:04:36 +0800

    test for sp-auth

--- apps/api/router.js
+++ apps/api/router.js
@@ -1,13 +1,13 @@
 const router = require('koa-router')()
 const instance = require('./instance')
-const { USER_STATUS, ROLE } = require('sp-auth/enum')
+const { USER_STATUS, ROLE, REGISTER_FROM } = require('sp-auth/enum')
 
 router
     .get('/register', async(ctx) => {
         let username = ctx.query.username
         let password = ctx.query.password
 
-        let user = await instance.authService.register({ role: ROLE.USER, status: USER_STATUS.ENABLE })
+        let user = await instance.authService.register({ role: ROLE.USER, status: USER_STATUS.ENABLE, from: REGISTER_FROM.WX })
         ctx.body = {
             code: 0,
             data: { user }
