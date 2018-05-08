2e6ac1e0d4d6134e9678af5b2ff4642b5442986e
dongwenxiao <cs_victor@126.com>
2018-05-08 16:49:29 +0800

    fix

--- src/webpack/prod.js
+++ src/webpack/prod.js
@@ -1,4 +1,11 @@
 module.exports = {
+    module: {
+        rules: [{
+            test: /\.(ico|gif|jpg|jpeg|png|svg|webp)$/,
+            loader: 'file-loader?context=static&name=assets/[hash:32].[ext]',
+            exclude: /node_modules/
+        }]
+    },
     output: {
         filename: `[name].[chunkhash].js`
     }
