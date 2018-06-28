d4bc72bf58dac78e6b3207cc31bb278a7f955334
Diablohu <Diablohu@users.noreply.github.com>
2018-06-28 11:46:25 +0800

    polish

--- src/app/lifecycle/on-history-update.js
+++ src/app/lifecycle/on-history-update.js
@@ -1,11 +1,4 @@
 export default (/*location, store*/) => {
-    // if (__DEV__) console.log('onHistoryUpdate')
-    // // 回调: browserHistoryOnUpdate
-    // // 正常路由跳转时，URL发生变化后瞬间会触发，顺序在react组件读取、渲染之前
-    // if (__DEV__) {
-    //     console.log('🌏 browserHistory update', location)
-    //     // console.log(actionUpdate(location))
-    // }
-    // store.dispatch(actionUpdate(location))
-    // // console.log(store.getState())
+    // 回调: 在浏览器里是更变时
+    // 在 onRouterUpdate 之前
 }
--- src/app/lifecycle/on-router-update.js
+++ src/app/lifecycle/on-router-update.js
@@ -1,18 +1,4 @@
-// import { onRouterChange } from '@appUI/layout/nav'
-
-// let isGAInit = false
-
 export default () => {
-    // if (__DEV__) console.log('router onUpdate', self.__LATHPATHNAME__, location.pathname)
-
-    // if (self.__LATHPATHNAME__)
-    //     lastScroll.set(self.__LATHPATHNAME__, window.scrollY)
-
-    // 统计代码第一次默认走html引入js
-    // if (typeof ga !== 'undefined' && isGAInit)
-    //     ga('send', 'pageview')
-
-    // isGAInit = true
-    // onRouterChange()
-    // self.__LATHPATHNAME__ = location.pathname
+    // 回调：路由更新完成时
+    // 在 onHistoryUpdate 之后
 }
